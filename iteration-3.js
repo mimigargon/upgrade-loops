5992a181300e90966326cae494bededdcb5b1fe8
mimigargon <mireiag77@gmail.com>
2022-05-17 14:05:14 +0200

    Iteration#3

---
+++ iteration-3.js
@@ -0,0 +1,6 @@
+const placesToTravel = ['Japon', 'Venecia', 'Murcia', 'Santander', 'Filipinas', 'Madagascar']
+
+for (const travel of placesToTravel) {
+    console.log("Nos vamos a", travel);
+  }
+  
