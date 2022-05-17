0b0722da1646d64f18b41853de2d33d0e595ab43
mimigargon <mireiag77@gmail.com>
2022-05-17 12:48:48 +0200

    Iteration#1

---
+++ iteration-1.js
@@ -0,0 +1,10 @@
+const products = ['Camiseta de Pokemon', 'Pantalón coquinero', 'Gorra de gansta', 'Camiseta de Basket', 'Cinrurón de Orión', 'AC/DC Camiseta']
+
+products.forEach(function(product){
+
+    if (product.toLowerCase().includes("camiseta")) {
+      console.log(product);
+    }
+  
+  });
+  
